01555451afdf278380382806c249e4769a7e494b
fernanda isabel <isa95910@hotmail.com>
2022-12-11 15:02:38 -0500

    first commit

---
+++ src/lista-restaurantes.js
@@ -0,0 +1,35 @@
+const lrestaurantes = [
+{
+    Id: 1,
+    Nombre: 'Pacifico Mar',
+    Dirección: 'Av 85 Nro. 45 - 85 Barranquilla',
+    Descripción: 'Deliciosa comida de mar',
+    Imagen: "https://th.bing.com/th/id/OIP.QBgIpPAWpUOwY4z4J51SCwHaFj?pid=ImgDet&rs=1"
+},
+{
+    Id: 2,
+    Nombre: 'Italiano 65',
+    Dirección: 'Av 45 Nro. 1 - 85 Medellín',
+    Descripción: 'Deliciosa comida italiana',
+    Imagen: "https://th.bing.com/th/id/OIP.GTU-0XnEO2HVu6bHCZHUMQHaEo?pid=ImgDet&rs=1"
+},
+{
+    Id: 3,
+    Nombre: 'Fritos y Mas',
+    Dirección: 'Calle 45 Medellín',
+    Descripción: 'Comida rápida',
+    Imagen: "https://th.bing.com/th/id/R.dd2c9c2e850674daa68c957eae15e563?rik=AVLX7MwlNcXNXQ&pid=ImgRaw&r=0&sres=1&sresct=1"
+},
+{
+    Id: 4,
+    Nombre: 'Pollo Frito',
+    Dirección: 'Cr 32 # 56 - 89',
+    Descripción: 'Lo mejor del pollo frito',
+    Imagen: "https://th.bing.com/th/id/R.42aa31fc62b27cf3346ea8141d43d3c9?rik=6UAktmG3VeugAA&pid=ImgRaw&r=0"
+}
+
+]
+
+export {
+    lrestaurantes
+}
